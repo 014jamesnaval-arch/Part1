ada0455dbb7d43ba2e25bc8e2ffa697a7d6a1afa
014jamesnaval-arch <014jamesnaval@gmail.com>
2026-04-15 18:58:25 +0800

    Add files via upload

---
+++ lib/providers/schedule_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../models/task_model.dart';
+import 'package:uuid/uuid.dart';
+// UUID = Universally Unique Identifier
+// Create a random strings "f47Ac10B-58cc-a5678-0785j3Gb79"
+class ScheduleProvider extends ChangeNotifier{
+    final List<TaskModel> _tasks =[];
+    final Uuid _uuid = const Uuid();
+
+    List<TaskModel> get tasks => _tasks;
+
+    void addTask ({
+
+        required String title, required String category, required DateTime date,
+        required TimeOfDay startTime,required TimeOfDay endTime, required int urgency,
+        required int importance, required double estimatedEffortHours, required String energyLevel,
+
+    }){
+    final newTask = TaskModel(
+        id : _uuid.v4(),
+        title: title,
+        category : category,
+        date : date,
+        startTime : startTime,
+        endTime : endTime,
+        urgency : urgency,
+        importance : importance,
+        estimatedEffortHours : estimatedEffortHours,
+        energyLevel : energyLevel,
+        );
+        _tasks.add(newTask);
+        notifyListeners();
+    }
+    void removeTasks(String id){
+        _tasks.removeWhere((task) => task.id ==id);
+        notifyListeners();
+
+    }
+}
